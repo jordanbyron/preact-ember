da654fd710cb02c318b87ff7506b93da6a1369e1
Jordan Byron <jordan.byron@gmail.com>
2019-03-13 06:57:26 -0400

    Lifecycle hooks

--- app/components/preact-demo.js
+++ app/components/preact-demo.js
@@ -3,6 +3,30 @@ import Clock from 'ember-quickstart/react-components/hello-world';
 import { h, render } from 'preact';
 
 export default Component.extend({
+  startClock() {
+    const timer = later(this, function() {
+      this.tick();
+    }, 1000);
+
+    this.set('timer', timer);
+  },
+
+  tick() {
+    this.set('time', new Date().toLocaleTimeString());
+  },
+
+  props: computed('time', function() {
+    return { time: this.time };
+  }),
+
+  didRender() {
+
+  },
+
+  willDestroyElement() {
+
+  },
+
   didInsertElement: function() {
     render(<Clock name={this.name} />, this.element);
   }
